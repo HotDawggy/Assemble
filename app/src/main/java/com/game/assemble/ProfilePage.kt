cda46b4fb4e84aace9952dfe6e8d81f98577898d
kennyhow <kennethyonghw@gmail.com>
2024-04-21 03:30:34 +0800

    Add sharedPrefs usage to profile page

--- app/src/main/java/com/game/assemble/ProfilePage.kt
+++ app/src/main/java/com/game/assemble/ProfilePage.kt
@@ -3,13 +3,40 @@ package com.game.assemble
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.internal.userAgent
 
 class ProfilePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_page)
+
+        val sharedPrefs = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+
+        // init username
+        val usernameEditText: EditText = findViewById(R.id.profilePageUsername)
+        val savedUsername = sharedPrefs.getString("username", "")
+        if (savedUsername == "") {
+            usernameEditText.setHint("Enter Username :3")
+        }
+        else {
+            usernameEditText.setText(savedUsername)
+        }
+
+        // init high score
+        val highScoreTextView: TextView = findViewById(R.id.profilePageHighScore)
+        highScoreTextView.text = "High Score: " + sharedPrefs.getString("highScore", "0")
+
+        // init games played
+        val gamesPlayedTextView: TextView = findViewById(R.id.profilePageGamesPlayedCount)
+        gamesPlayedTextView.text = "Total Games Played: " + sharedPrefs.getInt("gamesPlayed", 0).toString()
+
+        // init fav asm instruction
+        val famAsmTextView: TextView = findViewById(R.id.profilePageFavAsm)
+        famAsmTextView.text = "You used ${sharedPrefs.getString("favAsm", "XOR")} ${sharedPrefs.getInt("favAsmCount", 0)} times!"
 
         // from https://stackoverflow.com/questions/44148852/how-to-add-a-button-dynamically-in-android-using-kotlin
         val runHistoryLayout = findViewById<LinearLayout>(R.id.profilePageRunHistoryLayout)
